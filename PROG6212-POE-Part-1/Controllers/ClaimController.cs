8a857ee42fbacc43e5191499c1edd76c8d21f831
Vutlhari Mabasa <ST10380779@rcconnect.edu.za>
2024-09-03 17:08:16 +0000

    added claim controller and edited other pages

---
+++ PROG6212-POE-Part-1/Controllers/ClaimController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PROG6212_POE_Part_1.Models;
+
+namespace PROG6212_POE_Part_1.Controllers
+{
+    public class ClaimController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SubmitClaim()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitClaim(ClaimModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Process the claim (e.g., save to database)
+                // Redirect or return a success view
+                return RedirectToAction("VerifyClaims");
+            }
+
+            // If model validation fails, return the same view with the current model to show validation errors
+            return View(model);
+        }
+        public IActionResult TrackStatus()
+        {
+            return View();
+        }
+        public IActionResult VerifyClaims()
+        {
+            return View();
+        }
+    }
+}
---
+++ PROG6212-POE-Part-1/Models/ClaimModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PROG6212_POE_Part_1.Models
+{
+    public class ClaimModel
+    {
+        [Required(ErrorMessage = "Please provide a description of the claim")]
+        public string ClaimDescription { get; set; } // Description of the claim
+
+        [Required(ErrorMessage = "Please enter the date of the incident")]
+        [Display(Name = "Supporting Documents")]
+        public List<IFormFile> SupportingDocuments { get; set; } // List of uploaded files
+
+        public decimal ClaimAmount { get; set; } // The amount being claimed
+    }
+}
